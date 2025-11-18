763ea9d5f703478c421ff702839e7dbb44523a09
vlazed <35244845+vlazed@users.noreply.github.com>
2025-11-18 10:03:10 -0500

    Add lighting tuners + Add lift/gamma/gain parameters to change cel lighting

--- lua/postprocess/vlazed_posterize.lua
+++ lua/postprocess/vlazed_posterize.lua
@@ -8,6 +8,24 @@ local pp_posterize_g = CreateClientConVar("pp_vlazedposterize_g", "255", true, f
 local pp_posterize_b = CreateClientConVar("pp_vlazedposterize_b", "255", true, false, nil, 0, 255)
 local pp_posterize_a = CreateClientConVar("pp_vlazedposterize_a", "255", true, false, nil, 0, 255)
 
+local pp_posterize_lift_r = CreateClientConVar("pp_vlazedposterize_lift_r", "255", true, false, nil, 0, 255)
+local pp_posterize_lift_g = CreateClientConVar("pp_vlazedposterize_lift_g", "255", true, false, nil, 0, 255)
+local pp_posterize_lift_b = CreateClientConVar("pp_vlazedposterize_lift_b", "255", true, false, nil, 0, 255)
+local pp_posterize_lift_a = CreateClientConVar("pp_vlazedposterize_lift_a", "255", true, false, nil, 0, 255)
+local pp_posterize_lift_scale = CreateClientConVar("pp_vlazedposterize_lift", "1", true, false, nil, 0, 255)
+
+local pp_posterize_gamma_r = CreateClientConVar("pp_vlazedposterize_gamma_r", "255", true, false, nil, 0, 255)
+local pp_posterize_gamma_g = CreateClientConVar("pp_vlazedposterize_gamma_g", "255", true, false, nil, 0, 255)
+local pp_posterize_gamma_b = CreateClientConVar("pp_vlazedposterize_gamma_b", "255", true, false, nil, 0, 255)
+local pp_posterize_gamma_a = CreateClientConVar("pp_vlazedposterize_gamma_a", "255", true, false, nil, 0, 255)
+local pp_posterize_gamma_scale = CreateClientConVar("pp_vlazedposterize_gamma", "1", true, false, nil, 0, 255)
+
+local pp_posterize_gain_r = CreateClientConVar("pp_vlazedposterize_gain_r", "255", true, false, nil, 0, 255)
+local pp_posterize_gain_g = CreateClientConVar("pp_vlazedposterize_gain_g", "255", true, false, nil, 0, 255)
+local pp_posterize_gain_b = CreateClientConVar("pp_vlazedposterize_gain_b", "255", true, false, nil, 0, 255)
+local pp_posterize_gain_a = CreateClientConVar("pp_vlazedposterize_gain_a", "255", true, false, nil, 0, 255)
+local pp_posterize_gain_scale = CreateClientConVar("pp_vlazedposterize_gain", "1", true, false, nil, 0, 255)
+
 local hookName = "vlazed_posterize_hook"
 local width, height = ScrW(), ScrH()
 
@@ -20,6 +38,19 @@ function render.DrawVlazedPosterize()
 	mat_posterize:SetFloat("$c0_y", pp_posterize_r:GetFloat() / 255)
 	mat_posterize:SetFloat("$c0_z", pp_posterize_g:GetFloat() / 255)
 	mat_posterize:SetFloat("$c0_w", pp_posterize_b:GetFloat() / 255)
+
+	local liftScale = pp_posterize_lift_scale:GetFloat()
+	mat_posterize:SetFloat("$c1_x", pp_posterize_lift_r:GetFloat() / 255 * liftScale)
+	mat_posterize:SetFloat("$c1_y", pp_posterize_lift_g:GetFloat() / 255 * liftScale)
+	mat_posterize:SetFloat("$c1_z", pp_posterize_lift_b:GetFloat() / 255 * liftScale)
+	local gammaScale = pp_posterize_gamma_scale:GetFloat()
+	mat_posterize:SetFloat("$c2_x", pp_posterize_gamma_r:GetFloat() / 255 * gammaScale)
+	mat_posterize:SetFloat("$c2_y", pp_posterize_gamma_g:GetFloat() / 255 * gammaScale)
+	mat_posterize:SetFloat("$c2_z", pp_posterize_gamma_b:GetFloat() / 255 * gammaScale)
+	local gainScale = pp_posterize_gain_scale:GetFloat()
+	mat_posterize:SetFloat("$c3_x", pp_posterize_gain_r:GetFloat() / 255 * gainScale)
+	mat_posterize:SetFloat("$c3_y", pp_posterize_gain_g:GetFloat() / 255 * gainScale)
+	mat_posterize:SetFloat("$c3_z", pp_posterize_gain_b:GetFloat() / 255 * gainScale)
 	render.SetMaterial(mat_posterize)
 	render.DrawScreenQuad()
 end
@@ -69,6 +100,24 @@ list.Set("PostProcess", "Posterize (vlazed)", {
 			pp_vlazedposterize_g = 255,
 			pp_vlazedposterize_b = 255,
 			pp_vlazedposterize_a = 255,
+
+			pp_vlazedposterize_lift = 1,
+			pp_vlazedposterize_lift_r = 255,
+			pp_vlazedposterize_lift_g = 255,
+			pp_vlazedposterize_lift_b = 255,
+			pp_vlazedposterize_lift_a = 255,
+
+			pp_vlazedposterize_gamma = 1,
+			pp_vlazedposterize_gamma_r = 255,
+			pp_vlazedposterize_gamma_g = 255,
+			pp_vlazedposterize_gamma_b = 255,
+			pp_vlazedposterize_gamma_a = 255,
+
+			pp_vlazedposterize_gain = 1,
+			pp_vlazedposterize_gain_r = 255,
+			pp_vlazedposterize_gain_g = 255,
+			pp_vlazedposterize_gain_b = 255,
+			pp_vlazedposterize_gain_a = 255,
 			pp_vlazedposterize_celcount = 8.00,
 		}
 		CPanel:ToolPresets("vlazedposterize", options)
@@ -83,5 +132,33 @@ list.Set("PostProcess", "Posterize (vlazed)", {
 			"pp_vlazedposterize_a"
 		)
 		CPanel:NumSlider("Cel count", "pp_vlazedposterize_celcount", 1, 20, 0)
+
+		local lightSettings = makeCategory(CPanel, "Light Settings", "ControlPanel")
+		lightSettings:ColorPicker(
+			"Lift",
+			"pp_vlazedposterize_lift_r",
+			"pp_vlazedposterize_lift_g",
+			"pp_vlazedposterize_lift_b",
+			"pp_vlazedposterize_lift_a"
+		)
+		lightSettings:NumSlider("Lift Multiplier", "pp_vlazedposterize_lift", 0, 100, 5)
+
+		lightSettings:ColorPicker(
+			"Gamma",
+			"pp_vlazedposterize_gamma_r",
+			"pp_vlazedposterize_gamma_g",
+			"pp_vlazedposterize_gamma_b",
+			"pp_vlazedposterize_gamma_a"
+		)
+		lightSettings:NumSlider("Gamma Multiplier", "pp_vlazedposterize_gamma", 0, 100, 5)
+
+		lightSettings:ColorPicker(
+			"Gain",
+			"pp_vlazedposterize_gain_r",
+			"pp_vlazedposterize_gain_g",
+			"pp_vlazedposterize_gain_b",
+			"pp_vlazedposterize_gain_a"
+		)
+		lightSettings:NumSlider("Gain Multiplier", "pp_vlazedposterize_gain", 0, 100, 5)
 	end,
 })
--- shaders/vlazed_posterize_ps30.hlsl
+++ shaders/vlazed_posterize_ps30.hlsl
@@ -6,12 +6,37 @@ sampler COLORTEXTURE : register(s1);
 
 float4 CELS   : register(c0);
 
+float4 LIFT   : register(c1);
+float4 GAMMA   : register(c2);
+float4 GAIN   : register(c3);
+
 struct PS_INPUT
 {
     float2 uv : TEXCOORD0;             // Position on triangle
     float2 pPos: VPOS;
     float3 view_space_dir : TEXCOORD1; // Projection matrix (used for calculating depth)
 };
+
+// LiftGammaGain by 3an and CeeJay.dk
+// https://github.com/CeeJayDK/SweetFX/blob/16d1a42247cb5baaf660120ee35c9a33bb94649c/Shaders/SweetFX/LiftGammaGain.fx#L27
+float3 LiftGammaGainPass(float3 color)
+{
+    float3 RGB_Lift = LIFT.rgb;
+    float3 RGB_Gamma = GAMMA.rgb;
+    float3 RGB_Gain = GAIN.rgb;
+
+	// -- Lift --
+	color = color * (1.5 - 0.5 * RGB_Lift) + 0.5 * RGB_Lift - 0.5;
+	color = saturate(color); // Is not strictly necessary, but does not cost performance
+	
+	// -- Gain --
+	color *= RGB_Gain; 
+	
+	// -- Gamma --
+	color = pow(abs(color), 1.0 / RGB_Gamma);
+	
+	return saturate(color);
+}
 
 float4 main(PS_INPUT frag) : COLOR
 {
@@ -21,11 +46,13 @@ float4 main(PS_INPUT frag) : COLOR
     // Cel shading (really just a glorified, additive posterize effect for source) from 
     // https://martinwiddowson.artstation.com/blog/2Kpy/a-guide-to-screen-space-halftones-cel-shading-and-toon-shading-for-post-processing-in-unreal-engine-4-20-3
     float intensity = dot(diffuse.rgb, float3(0.2126, 0.7152, 0.0722));
-    float cel = floor(CELS.x * intensity) / CELS.x;
+    float3 intensity3 = LiftGammaGainPass(intensity);
+
+    float3 cel = floor(CELS.x * intensity3) / CELS.x;
     float3 colorMult = CELS.yzw;
     color.rgb *= colorMult;
 
-    float4 result = color + cel;
+    float4 result = float4(color + cel, 1);
 
     return result;
 }
